833a1f5b92096ec98fb610c1398ed4d3d171fc15
Minchul An <min@minchulan.io>
2023-04-06 14:38:20 -0600

    commit tree c3eef05614978956c412770f67d6ef7c558e1d26

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # mapped to the cheeses controller and its index action that renders an array of cheeses as JSON
   get '/cheeses', to: 'cheeses#index'
+
+  get '/cheeses/:id', to: 'cheeses#show'
+
 end
